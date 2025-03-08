c1c56fe713c4567461fd7b08c7379723f80f09d4
seanyew25 <seanyew25@gmail.com>
2025-03-08 14:26:03 +0800

    Added new API endpoints and added new query parameters for existing ones
    - Added POST endpoint to create delivery
    - Added capability for GET deliveries to query by start and end time, by order_id and by driver_id

--- delivery/delivery.py
+++ delivery/delivery.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 import os
 from dotenv import load_dotenv, dotenv_values
-import psycopg2
+from datetime import datetime, timezone
 
 app = Flask(__name__)
 
@@ -31,15 +31,16 @@ class Delivery(db.Model):
     start_time = db.Column(db.TIMESTAMP(timezone=True), nullable=False)
     end_time = db.Column(db.TIMESTAMP(timezone=True), nullable=False)
     location = db.Column(db.Text, nullable=False)
+    driver_id = db.Column(db.Integer, nullable=False)
 
 
 
-    def __init__(self, id, order_id, start_time, end_time, location):
-        self.id = id
+    def __init__(self, order_id, start_time, end_time, location, driver_id):
         self.order_id = order_id
         self.start_time = start_time
         self.end_time = end_time
         self.location = location
+        self.driver_id = driver_id
 
 
     def json(self):
@@ -48,13 +49,15 @@ class Delivery(db.Model):
             "order_id": self.order_id,
             "start_time": self.start_time,
             "end_time": self.end_time,
+            "driver_id": self.driver_id,
             "location": self.location,
         }
 
-
-@app.route("/delivery")
-def get_all():
-    delivery_list = db.session.scalars(db.select(Delivery)).all()
+def get_delivery_by_time(start_time, end_time):
+    start_time = datetime.fromtimestamp(int(start_time), timezone.utc)
+    end_time = datetime.fromtimestamp(int(end_time), timezone.utc)
+    print(start_time, end_time)
+    delivery_list = db.session.scalars(db.select(Delivery).filter(Delivery.start_time >= start_time, Delivery.end_time <= end_time)).all()
 
     if len(delivery_list):
         return jsonify(
@@ -63,52 +66,87 @@ def get_all():
                 "data": {"deliveries": [delivery.json() for delivery in delivery_list]},
             }
         )
-    return jsonify({"code": 404, "message": "There are no deliveries."}), 404
+    return jsonify({"code": 404, "message": "There are no deliveries for this time period."}), 404
+
+def get_delivery_by_driver(driver_id):
+    delivery_list = db.session.scalars(db.select(Delivery).filter(Delivery.driver_id == driver_id)).all()
+
+    if len(delivery_list):
+        return jsonify(
+            {
+                "code": 200,
+                "data": {"deliveries": [delivery.json() for delivery in delivery_list]},
+            }
+        )
+    return jsonify({"code": 404, "message": "There are no deliveries for this driver."}), 404
+
+def get_delivery_by_order(order_id):
+    delivery_list = db.session.scalars(db.select(Delivery).filter(Delivery.order_id == order_id)).all()
+    if len(delivery_list):
+        return jsonify(
+            {
+                "code": 200, 
+                "data": {"deliveries": [delivery.json() for delivery in delivery_list]}
+        }
+    )
+    return jsonify({"code": 404, "message": "Delivery not found."}), 404
+
+@app.route("/delivery")
+def get_deliveries():
+    if request.args.get("start_time") and request.args.get("end_time"):
+        start_time = request.args.get("start_time")
+        end_time = request.args.get("end_time")
+        print(start_time, end_time)
+        return get_delivery_by_time(start_time, end_time)
+
+    elif request.args.get("driver_id"):
+        driver_id = request.args.get("driver_id")
+        return get_delivery_by_driver(driver_id)
+    
+    elif request.args.get("order_id"):
+        order_id = request.args.get("order_id")
+        return get_delivery_by_order(order_id)
+    
+    else:
+        delivery_list = db.session.scalars(db.select(Delivery)).all()
+
+        if len(delivery_list):
+            return jsonify(
+                {
+                    "code": 200,
+                    "data": {"deliveries": [delivery.json() for delivery in delivery_list]},
+                }
+            )
+        return jsonify({"code": 404, "message": "There are no deliveries."}), 404
 
 
-# @app.route("/book/<string:isbn13>")
-# def find_by_isbn13(isbn13):
-#     book = db.session.scalar(db.select(Book).filter_by(isbn13=isbn13))
-
-#     if book:
-#         return jsonify({"code": 200, "data": book.json()})
-#     return jsonify({"code": 404, "message": "Book not found."}), 404
 
 
-# @app.route("/book/<string:isbn13>", methods=["POST"])
-# def create_book(isbn13):
-#     if db.session.scalar(db.select(Book).filter_by(isbn13=isbn13)):
-#         return (
-#             jsonify(
-#                 {
-#                     "code": 400,
-#                     "data": {"isbn13": isbn13},
-#                     "message": "Book already exists.",
-#                 }
-#             ),
-#             400,
-#         )
+@app.route("/delivery", methods=["POST"])
+def create_delivery():
 
-#     data = request.get_json()
-#     book = Book(isbn13, **data)
+    data = request.get_json()
+    delivery = Delivery(**data)
 
-#     try:
-#         db.session.add(book)
-#         db.session.commit()
-#     except Exception as e:
-#         print("Exception:{}".format(str(e)))
-#         return (
-#             jsonify(
-#                 {
-#                     "code": 500,
-#                     "data": {"isbn13": isbn13},
-#                     "message": "An error occurred creating the book.",
-#                 }
-#             ),
-#             500,
-#         )
+    try:
+        db.session.add(delivery)
+        db.session.commit()
+    except Exception as e:
+        print("Exception:{}".format(str(e)))
+        return (
+            jsonify(
+                {
+                    "code": 500,
+                    "message": "An error occurred creating the delivery.",
+                }
+            ),
+            500,
+        )
 
-#     return jsonify({"code": 201, "data": book.json()}), 201
+    return jsonify({"code": 201, "data": delivery.json()}), 201
+
+
+
 
 
 if __name__ == "__main__":
