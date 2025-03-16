82de67685d39a53f3379dbeb3f25e671e89de783
Darryl <darryltan.2023@smu.edu.sg>
2025-03-16 16:32:02 +0800

    Update of UI Driver

--- ui_driver/src/app/login/page.tsx
+++ ui_driver/src/app/login/page.tsx
@@ -11,7 +11,7 @@ export default function LoginPage() {
             <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
               <GalleryVerticalEnd className="size-4" />
             </div>
-            Acme Inc.
+            NomNomBox
           </a>
         </div>
         <div className="flex flex-1 items-center justify-center">
@@ -22,7 +22,7 @@ export default function LoginPage() {
       </div>
       <div className="bg-muted relative hidden lg:block">
         <img
-          src="/placeholder.jpg"
+          src="https://img.freepik.com/free-photo/close-up-hands-holding-food-pack_23-2149182237.jpg?t=st=1742107228~exp=1742110828~hmac=d9bc537643c89f5d5d2ef6c70c3baed0bb41ebcbabb2b9bc99fd7b3765ed4402&w=1380"
           alt="Meal Kit Delivery"
           className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
         />
