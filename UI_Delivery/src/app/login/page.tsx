627cc6e6e85b6b22a699cba16af2a51d70e9483f
Darryl <darryltan.2023@smu.edu.sg>
2025-03-29 01:24:53 +0800

    edited login page

--- UI_Delivery/src/app/login/page.tsx
+++ UI_Delivery/src/app/login/page.tsx
@@ -1,31 +1,9 @@
-import { GalleryVerticalEnd } from "lucide-react"
-
 import { LoginForm } from "@/components/login-form"
-
 export default function LoginPage() {
   return (
-    <div className="grid min-h-svh lg:grid-cols-2">
-      <div className="flex flex-col gap-4 p-6 md:p-10">
-        <div className="flex justify-center gap-2 md:justify-start">
-          <a href="#" className="flex items-center gap-2 font-medium">
-            <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-              <GalleryVerticalEnd className="size-4" />
-            </div>
-            NomNomBox
-          </a>
-        </div>
-        <div className="flex flex-1 items-center justify-center">
-          <div className="w-full max-w-xs">
-            <LoginForm />
-          </div>
-        </div>
-      </div>
-      <div className="bg-muted relative hidden lg:block">
-        <img
-          src="https://img.freepik.com/free-photo/close-up-hands-holding-food-pack_23-2149182237.jpg?t=st=1742107228~exp=1742110828~hmac=d9bc537643c89f5d5d2ef6c70c3baed0bb41ebcbabb2b9bc99fd7b3765ed4402&w=1380"
-          alt="Meal Kit Delivery"
-          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-        />
+    <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-3xl">
+        <LoginForm />
       </div>
     </div>
   )
